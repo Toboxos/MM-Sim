99d36d3452735d3109aa84f8693ceee278b0dc19
Tobias Bungard <tobi-bungard@t-online.de>
2020-02-26 23:20:47 +0000

    Fill M&M to colorwheel

--- server.py
+++ server.py
@@ -5,6 +5,7 @@ import websockets
 import json
 import threading
 import time
+import random
 
 from state import readState, printState
 import RPi.GPIO as GPIO
@@ -36,8 +37,22 @@ SLP = 27
 
 
 # Control Variables
-angleOut = 0
+angleOut = 120
 angleCw = 45
+
+
+# Color definitions
+NONE = 0
+RED = 1
+GREEN = 2
+BLUE = 3
+BROWN = 4
+ORANGE = 5
+YELLOW = 6
+COLORS = [RED, GREEN, BLUE, BROWN, ORANGE, YELLOW]
+
+# M&Ms in ColorWheel
+cw_mm = [NONE, NONE, NONE, NONE]
 
 sockets = []
 async def hello(websocket, path):
@@ -52,7 +67,8 @@ async def update_loop():
     while True:
         msg = {
             'angleOut': angleOut,
-            'angleCw': angleCw
+            'angleCw': angleCw,
+            'mm': cw_mm
         }
 
         for s in sockets:
@@ -70,28 +86,55 @@ def doStep(channel):
 
     # Step colorwheel
     if channel == STEP_CW:
-        angleCw += 360 / 1600   # 1600 Steps for 360°
+        if GPIO.input(DIR_CW) == 0:
+            angleCw += 360 / 1600   # 1600 Steps for 360°
+        else:
+            angleCw -= 360 / 1600
+
         if angleCw > 360:
             angleCw -= 360
-        updateSensors()
+        elif angleCw < 0:
+            angleCw += 360
 
     elif channel == STEP_OUT:
-        angleOut += 360 / 400   # 400 Steps for 360°
+        if GPIO.input(DIR_OUT) == 0:
+            angleOut += 360 / 400    # 400 Steps for 360°
+        else:
+            angleOut -= 360 / 400
+
         if angleOut > 360:
             angleOut -= 360
-        updateSensors()
+        elif angleOut < 0:
+            angleOut += 360
+
+    updateSensors()
 
 def updateSensors():
 
     # Colorwheel Hallsensor
     if angleCw > 358 or angleCw < 2:        #   0deg
-        GPIO.output( HALL_CW, 0 )
+        GPIO.output( HALL_CW, 0 )# Color definitions
+
+        if cw_mm[0] == NONE:
+            cw_mm[0] = COLORS[random.randint(0, 5)]
+            
     elif angleCw > 88 and angleCw < 92:     #  90deg
         GPIO.output( HALL_CW, 0)
+
+        if cw_mm[3] == NONE:
+            cw_mm[3] = COLORS[random.randint(0, 5)]
+        
     elif angleCw > 178 and angle < 182:     # 180deg
         GPIO.output( HALL_CW, 0)
+
+        if cw_mm[2] == NONE:
+            cw_mm[2] = COLORS[random.randint(0, 5)]
+        
     elif angleCw > 268 and angle < 272:     # 270deg
         GPIO.output( HALL_CW, 0 )
+
+        if cw_mm[1] == NONE:
+            cw_mm[1] = COLORS[random.randint(0, 5)]
     else:
         GPIO.output( HALL_CW, 1 )
 
@@ -122,6 +165,9 @@ GPIO.setup( HALL_OUT, GPIO.OUT )
 GPIO.setup( STEP_CW, GPIO.IN )
 GPIO.setup( STEP_OUT, GPIO.IN )
 
+# Motor direction as input
+GPIO.setup( DIR_CW, GPIO.IN )
+GPIO.setup( DIR_OUT, GPIO.IN )
 
 
 # Stepping colorwheel & outlet
